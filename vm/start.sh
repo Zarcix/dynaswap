acbf28efca7c23eb0d9ab912325255c65cd4a68d
dalamity <mrslime43@gmail.com>
2026-08-28 15:00:37 -0700

    Add qemu machine flags

--- vm/start.sh
+++ vm/start.sh
@@ -2,7 +2,11 @@
 
 export IMAGE_NAME="fs.qcow2"
 
-qemu-system-x86_64 -enable-kvm -cpu host -smp 2 -m 2G \
+qemu-system-x86_64 \
+  -machine q35,accel=kvm \
+  -cpu host \
+  -smp 1 \
+  -m 1G \
   -drive file=${IMAGE_NAME},format=qcow2,if=virtio,cache=none,aio=native \
   -net nic -net user \
   -virtfs local,path=../src,mount_tag=myshare,security_model=mapped-xattr \

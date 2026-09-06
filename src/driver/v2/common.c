e28d757fff788c2d63fed45cbcc10d30f037e562
dalamity <mrslime43@gmail.com>
2026-09-06 16:50:31 -0700

    Add base sysfs support

--- src/driver/v2/common.c
+++ src/driver/v2/common.c
@@ -1,17 +1,22 @@
 #include "common.h"
 
+#include <linux/debugfs.h>
 #include <linux/workqueue.h>
 
 struct workqueue_struct *DYNASWAP_WORKQUEUE = NULL;
+struct dentry *DYNASWAP_SYSFS_DIR = NULL;
 
 int setup_common(void) {
     DYNASWAP_WORKQUEUE = alloc_workqueue("dynaswap_wq", WORKQUEUE_FLAGS, 0);
     if (!DYNASWAP_WORKQUEUE) {
         return -ENOMEM;
     }
+
+    DYNASWAP_SYSFS_DIR = debugfs_create_dir("dynaswap", NULL);
     return 0;
 }
 
 void teardown_common(void) {
+    debugfs_remove_recursive(DYNASWAP_SYSFS_DIR);
     destroy_workqueue(DYNASWAP_WORKQUEUE);
 }
--- src/driver/v2/common.h
+++ src/driver/v2/common.h
@@ -8,13 +8,15 @@
 #define log_info(fmt, ...) pr_info("dynaswap::%s -- " fmt "\n", __FILE__, ##__VA_ARGS__)
 #define log_alert(fmt, ...) pr_alert("dynaswap::%s -- " fmt "\n", __FILE__, ##__VA_ARGS__)
 
-#include <linux/printk.h>
 #include <linux/compiler.h>
+#include <linux/debugfs.h>
+#include <linux/printk.h>
 #include <linux/types.h>
 
 #define WORKQUEUE_FLAGS (WQ_MEM_RECLAIM | WQ_HIGHPRI | WQ_UNBOUND)
 
 extern struct workqueue_struct *DYNASWAP_WORKQUEUE;
+extern struct dentry *DYNASWAP_SYSFS_DIR;
 
 int setup_common(void);
 void teardown_common(void);
